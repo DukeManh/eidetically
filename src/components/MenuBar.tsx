10689bf2d9238ab9bd05d78b9528d245b4cc39c6
DukeManh <manhducdkcb@gmail.com>
2021-05-21 16:52:13 -0400

    Menu bar styles

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -2,15 +2,15 @@ import { BsDropletHalf } from 'react-icons/bs';
 
 export default function MenuBar() {
   return (
-    <nav className="h-8 bg-secondary text-gray-100 border-b border-gray-500 z-100">
-      <ul className="menubar pl-2 flex flex-row justify-start align-middle gap-x-4">
-        <li>
-          <BsDropletHalf className="h-full" />
-        </li>
-        <li>File</li>
-        <li>Layout</li>
-        <li>About</li>
-      </ul>
+    <nav className="h-9 bg-secondary text-gray-100 border-b border-gray-500 z-100">
+      <span className="align-text-top menubar pl-2 inline-flex flex-row justify-start items-center">
+        <button>
+          <BsDropletHalf />
+        </button>
+        <button>File</button>
+        <button>Layout</button>
+        <button>About</button>
+      </span>
     </nav>
   );
 }
