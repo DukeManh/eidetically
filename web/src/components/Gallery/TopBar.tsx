0487fceca7f70cd0a58ae33163256cdb4ef435b0
DukeManh <manhducdkcb@gmail.com>
2021-08-08 17:57:14 -0400

    Topbar seach box

--- web/src/components/Gallery/TopBar.tsx
+++ web/src/components/Gallery/TopBar.tsx
@@ -1,5 +1,4 @@
-import { HiMenuAlt2, HiMenuAlt3 } from 'react-icons/hi';
-// import { HiOutlinePlusCircle, HiOutlineMinusCircle } from 'react-icons/hi';
+import { HiMenuAlt2, HiMenuAlt3, HiPlusSm, HiMinusSm, HiOutlineSearch } from 'react-icons/hi';
 
 import { useLayout, useLibrary } from '../../contexts';
 
@@ -9,22 +8,22 @@ export default function TopBar() {
 
   return (
     <div className="md:px-4 min-h-[6rem] max-h-24 h-10 top-0 space-y-2">
-      <div className="h-1/2 flex flex-row justify-between items-center">
-        <HiMenuAlt2
-          size={28}
-          className="cursor-pointer"
-          onClick={() => {
-            updateNavigation({ visible: !navigation.visible });
-          }}
-        />
+      <div className="h-3/4 flex flex-row justify-between">
+        <div className="topbarColumn justify-start space-x-4">
+          <HiMenuAlt2
+            size={28}
+            className="cursor-pointer"
+            onClick={() => {
+              updateNavigation({ visible: !navigation.visible });
+            }}
+          />
+          <div className="text-md text-truncate">{activeLibrary?.name || 'Drop it'}</div>
+        </div>
 
-        <div className="text-md text-truncate max-w-[40%]">{activeLibrary?.name || 'Drop it'}</div>
-
-        <div className="flex flex-row items-center justify-evenly space-x-2">
-          {/* <HiOutlineMinusCircle
-            className="flex-shrink-0 cursor-pointer transform transition-transform duration-75 active:scale-125"
-            onClick={() => setZoom(zoom - 100)}
-          /> */}
+        <div className="topbarColumn justify-center space-x-2">
+          <div className="cursor-pointer p-1 rounded-full active:bg-tabActive">
+            <HiMinusSm onClick={() => setZoom(zoom - 100)} />
+          </div>
           <input
             className="zoom-slider"
             type="range"
@@ -34,21 +33,33 @@ export default function TopBar() {
             max={900}
             step={50}
           ></input>
-          {/* <HiOutlinePlusCircle
-            className="flex-shrink-0 cursor-pointer transform transition-transform duration-75 active:scale-125"
-            onClick={() => setZoom(zoom + 100)}
-          /> */}
+          <div className="cursor-pointer p-1 rounded-full active:bg-tabActive">
+            <HiPlusSm onClick={() => setZoom(zoom + 100)} />
+          </div>
         </div>
 
-        <div>Search</div>
+        <div className="topbarColumn justify-end space-x-4">
+          <div>
+            <form className="relative">
+              <div className="absolute h-full bg-tabFocus transform rounded-l-md w-8 flex flex-row justify-center items-center cursor-pointer active:bg-tabActive">
+                <HiOutlineSearch />
+              </div>
+              <input
+                className="bg-textArea box-border focus:border border-alert py-2 pr-3 pl-11 rounded-md caret-gray-400"
+                type="text"
+                placeholder="Search"
+              ></input>
+            </form>
+          </div>
 
-        <HiMenuAlt3
-          size={28}
-          className="cursor-pointer"
-          onClick={() => {
-            updateProperties({ visible: !properties.visible });
-          }}
-        />
+          <HiMenuAlt3
+            size={28}
+            className="cursor-pointer"
+            onClick={() => {
+              updateProperties({ visible: !properties.visible });
+            }}
+          />
+        </div>
       </div>
     </div>
   );
