deb9c29548a7df13453e62b12a56c8aac75be09c
Duke Manh <manhducdkcb@gmail.com>
2021-12-31 02:22:04 +0000

    smooth login popup animation

--- web/src/components/Login/LoginPopup.tsx
+++ web/src/components/Login/LoginPopup.tsx
@@ -11,12 +11,8 @@ import Mask from '../Mask';
 const transitionStyles: {
   [state: string]: CSSProperties;
 } = {
-  entering: { opacity: 0, transform: 'scale(0.7)', top: '20%', left: '-20%' },
-  entered: { opacity: 1, transform: 'scale(1)', top: 0, left: 0 },
-  exiting: { opacity: 1 },
-  exited: {
-    opacity: 0,
-  },
+  entering: { opacity: 0, transform: 'scale(0.6) translate(-30%, 30%)' },
+  exiting: { opacity: 0 },
 };
 
 const authUIConfig = {
@@ -44,42 +40,43 @@ export default function LoginPopup() {
   return (
     <>
       <Mask visible={loginVisible} zIndex={100} />
-      <Transition in={loginVisible} timeout={0} unmountOnExit enter appear exit>
-        {(state) => {
-          console.log(transitionStyles[state]);
-          return (
-            <div
-              className="fixed h-full w-full flex justify-center items-center z-[100]"
-              style={{
-                ...transitionStyles[state],
-                transitionProperty: 'top, left, opacity, transform',
-                transitionDuration: '300ms',
-                transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-              }}
-            >
-              <div className="relative py-6 w-72 h-72  rounded-md shadow-lg bg-white text-primary">
-                <p className="text-center font-bold text-xl">Sign in</p>
-                <div className="google-login mt-y"></div>
-                <GrClose
-                  className="absolute top-2 right-2 cursor-pointer"
-                  onClick={() => setLoginVisible(false)}
-                />
-                <div>
-                  <p className="agreements">
-                    By continuing, you are indicating that you accept Google's{' '}
-                    <a href="https://policies.google.com/terms" target="_blank" rel="noreferrer">
-                      Terms of Service
-                    </a>{' '}
-                    and{' '}
-                    <a href="https://policies.google.com/privacy" target="_blank" rel="noreferrer">
-                      Privacy Policy
-                    </a>
-                  </p>
-                </div>
+      <Transition in={loginVisible} timeout={200} unmountOnExit exit enter>
+        {(state) => (
+          <div
+            className="fixed h-full w-full flex justify-center items-center z-[100]"
+            style={{
+              transitionProperty: 'transform, opacity',
+              transitionDuration: '200ms',
+              transitionTimingFunction: 'ease-in-out',
+              opacity: 1,
+              transform: 'translate(0, 0) scale(1) ',
+              ...transitionStyles[state],
+            }}
+          >
+            <div className="relative py-6 w-72 h-72  rounded-md shadow-lg bg-white text-primary">
+              <p className="text-center font-bold text-xl">Sign in</p>
+              <div className="google-login mt-y"></div>
+              <button
+                className="absolute top-2 right-2 buttonIcon p-1"
+                onClick={() => setLoginVisible(false)}
+              >
+                <GrClose className="cursor-pointer" />
+              </button>
+              <div>
+                <p className="agreements">
+                  By continuing, you are indicating that you accept Google's{' '}
+                  <a href="https://policies.google.com/terms" target="_blank" rel="noreferrer">
+                    Terms of Service
+                  </a>{' '}
+                  and{' '}
+                  <a href="https://policies.google.com/privacy" target="_blank" rel="noreferrer">
+                    Privacy Policy
+                  </a>
+                </p>
               </div>
             </div>
-          );
-        }}
+          </div>
+        )}
       </Transition>
     </>
   );
